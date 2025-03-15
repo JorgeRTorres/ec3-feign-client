c099ddf6a75cc98c20b911f75b69033eea5ffdc9
JorgeRTorres <jorgetorres.0996@gmail.com>
2025-03-15 10:30:27 -0500

    service-client

---
+++ src/main/java/pe/edu/idat/ec3_feign_client/client/placeholder/iuserservice/PersonajeClient.java
@@ -0,0 +1,17 @@
+package pe.edu.idat.ec3_feign_client.client.placeholder.iuserservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "personajeClient",url = "https://rickandmortyapi.com")
+public interface PersonajeClient {
+
+    @GetMapping("/api/character")
+    List<Personaje> findAll();
+
+    @GetMapping("/api/character/{id}")
+    Personaje findById(@PathVariable("id") Long id);
+}
