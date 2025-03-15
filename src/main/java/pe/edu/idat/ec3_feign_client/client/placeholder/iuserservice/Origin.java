325829996661ea0038ada3f5f49658f47783d010
JorgeRTorres <jorgetorres.0996@gmail.com>
2025-03-15 10:12:44 -0500

    models

---
+++ src/main/java/pe/edu/idat/ec3_feign_client/client/placeholder/iuserservice/Origin.java
@@ -0,0 +1,22 @@
+package pe.edu.idat.ec3_feign_client.client.placeholder.iuserservice;
+
+public class Origin {
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/pe/edu/idat/ec3_feign_client/client/placeholder/iuserservice/Personaje.java
@@ -0,0 +1,76 @@
+package pe.edu.idat.ec3_feign_client.client.placeholder.iuserservice;
+
+public class Personaje {
+    private Long id;
+    private String name;
+    private String status;
+    private String species;
+    private String type;
+    private String gender;
+    private Origin origin;
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Origin getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Origin origin) {
+        this.origin = origin;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
