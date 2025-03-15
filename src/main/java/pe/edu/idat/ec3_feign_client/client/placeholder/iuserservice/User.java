2c41e43488d0563754135e89336db96680c955f4
JorgeRTorres <jorgetorres.0996@gmail.com>
2025-03-15 09:51:11 -0500

    init2

---
+++ src/main/java/pe/edu/idat/ec3_feign_client/client/placeholder/iuserservice/User.java
@@ -0,0 +1,4 @@
+package pe.edu.idat.ec3_feign_client.client.placeholder.iuserservice;
+
+public class User {
+}
---
+++ src/main/java/pe/edu/idat/ec3_feign_client/controller/UserController.java
@@ -0,0 +1,4 @@
+package pe.edu.idat.ec3_feign_client.controller;
+
+public class UserController {
+}
